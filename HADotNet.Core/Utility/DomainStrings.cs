9ba7217e475dde01a1dc842147d8bc3cc044308c
DSikes <pockybum522@gmail.com>
2020-11-04 20:38:10 -0500

    Added Domain class
    Domain contains a property for common HA domains, so you don't have to remember the domain strings. Useful for EntityClient.GetEntities

---
+++ HADotNet.Core/Utility/DomainStrings.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HADotNet.Core
+{
+    /// <summary>
+    /// A dictionary class consisting of HA domain strings
+    /// </summary>
+    public static class Domain
+    {
+        /// <summary>
+        /// "alarm_control_panel" string for the domain in Home Assistant
+        /// </summary>
+        public static string AlarmControlPanel => "alarm_control_panel";
+
+        /// <summary>
+        /// "binary_sensor" string for the domain in Home Assistant
+        /// </summary>
+        public static string BinarySensor => "binary_sensor";
+
+        /// <summary>
+        /// "camera" string for the domain in Home Assistant
+        /// </summary>
+        public static string Camera => "camera";
+
+        /// <summary>
+        /// "climate" string for the domain in Home Assistant
+        /// </summary>
+        public static string Climate => "climate";
+
+        /// <summary>
+        /// "cover" string for the domain in Home Assistant
+        /// </summary>
+        public static string Cover => "cover";
+
+        /// <summary>
+        /// "device_tracker" string for the domain in Home Assistant
+        /// </summary>
+        public static string DeviceTracker => "device_tracker";
+
+        /// <summary>
+        /// "fan" string for the domain in Home Assistant
+        /// </summary>
+        public static string Fan => "fan";
+
+        /// <summary>
+        /// "humidifier" string for the domain in Home Assistant
+        /// </summary>
+        public static string Humidifier => "humidifier";
+
+        /// <summary>
+        /// "light" string for the domain in Home Assistant
+        /// </summary>
+        public static string Light => "light";
+
+        /// <summary>
+        /// "lock" string for the domain in Home Assistant
+        /// </summary>
+        public static string Lock => "lock";
+
+        /// <summary>
+        /// "media_player" string for the domain in Home Assistant
+        /// </summary>
+        public static string MediaPlayer => "media_player";
+
+        /// <summary>
+        /// "sensor" string for the domain in Home Assistant
+        /// </summary>
+        public static string Sensor => "sensor";
+
+        /// <summary>
+        /// "Switch" string for the domain in Home Assistant
+        /// </summary>
+        public static string Switch => "switch";
+
+        /// <summary>
+        /// "WaterHeater" string for the domain in Home Assistant
+        /// </summary>
+        public static string WaterHeater => "water_heater";
+    }
+}
