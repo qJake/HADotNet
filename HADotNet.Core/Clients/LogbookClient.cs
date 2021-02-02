4a37898314bd58479bb0b6b3ce4517d387880943
Ihar Malkevich <imalkevic@gmail.com>
2021-02-02 19:42:16 +0200

    HADotNet - fix for empty sequence exception

--- HADotNet.Core/Clients/LogbookClient.cs
+++ HADotNet.Core/Clients/LogbookClient.cs
@@ -49,13 +49,13 @@ namespace HADotNet.Core.Clients
         /// <param name="startDate">The earliest history entry to retrieve.</param>
         /// <param name="endDate">The most recent history entry to retrieve.</param>
         /// <returns>A <see cref="LogbookObject"/> of history snapshots for the specified <paramref name="entityId" />, from <paramref name="startDate" /> to <paramref name="endDate" />.</returns>
-        public async Task<LogbookObject> GetLogbookEntries(string entityId, DateTimeOffset startDate, DateTimeOffset endDate) => (await Get<List<LogbookObject>>($"/api/logbook/{startDate.UtcDateTime:yyyy-MM-dd\\THH:mm:ss\\+00\\:00}?entity={entityId}&end_time={Uri.EscapeDataString(endDate.UtcDateTime.ToString("yyyy-MM-dd\\THH:mm:ss\\+00\\:00"))}")).First();
+        public async Task<LogbookObject> GetLogbookEntries(string entityId, DateTimeOffset startDate, DateTimeOffset endDate) => (await Get<List<LogbookObject>>($"/api/logbook/{startDate.UtcDateTime:yyyy-MM-dd\\THH:mm:ss\\+00\\:00}?entity={entityId}&end_time={Uri.EscapeDataString(endDate.UtcDateTime.ToString("yyyy-MM-dd\\THH:mm:ss\\+00\\:00"))}")).FirstOrDefault();
 
         /// <summary>
         /// Retrieves a list of historical states for the specified <paramref name="entityId" /> for the past 1 day.
         /// </summary>
         /// <param name="entityId">The entity ID to retrieve state history for.</param>
         /// <returns>A <see cref="LogbookObject"/> representing a 24-hour history snapshot for the specified <paramref name="entityId" />.</returns>
-        public async Task<LogbookObject> GetLogbookEntries(string entityId) => (await Get<List<LogbookObject>>($"/api/logbook?entity={entityId}")).First();
+        public async Task<LogbookObject> GetLogbookEntries(string entityId) => (await Get<List<LogbookObject>>($"/api/logbook?entity={entityId}")).FirstOrDefault();
     }
 }
